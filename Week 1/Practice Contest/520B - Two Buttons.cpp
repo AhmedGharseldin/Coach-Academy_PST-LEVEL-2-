5eda763dccc6f5908e0dc9e60f08d69a5f973e30
Ahmed Gharseldin <63879928+AhmedGharseldin@users.noreply.github.com>
2021-02-24 23:34:38 +0400

    init

---
+++ Week 1/Practice Contest/520B - Two Buttons.cpp
@@ -0,0 +1,34 @@
+//https://codeforces.com/problemset/problem/520/B
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int counter = 0;
+
+void solve(int m, int n);
+
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    solve(m, n);
+    cout << counter;
+}
+
+void solve(int m, int n)
+{
+    if (n < 0)
+        return;
+    if (n == m)
+        return;
+    if (n % 2 == 0 && n > m)
+    {
+        solve(m, n / 2);
+        counter++;
+    }
+    else
+    {
+        solve(m, n + 1);
+        counter++;
+    }
+}
