0eaa89d958343d15008e18f77b2fb0b10431ec7e
Ahmed Gharseldin <63879928+AhmedGharseldin@users.noreply.github.com>
2021-02-28 23:02:46 +0400

    init

---
+++ Week 2/Practice Contest/579A - Raising Bacteria.cpp
@@ -0,0 +1,23 @@
+//https://codeforces.com/problemset/problem/579/A
+
+#define FastIO() ios_base::sync_with_stdio(false), cin.tie(NULL);
+#include <bits/stdc++.h>
+typedef long long ll;
+
+using namespace std;
+const int N = 2e5 + 9;
+int arr[N];
+
+int main()
+{
+    int x, counter = 0;
+    cin >> x;
+    while (x)
+    {
+        if (x & 1)
+            counter++;
+        x >>= 1;
+    }
+    cout << counter << endl;
+    return 0;
+}
