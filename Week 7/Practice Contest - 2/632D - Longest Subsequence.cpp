2e33f4d9fc2be204b2ff0f45b06757d14394c103
Ahmed Gharseldin <63879928+AhmedGharseldin@users.noreply.github.com>
2021-07-07 19:21:33 +0400

    init

---
+++ Week 7/Practice Contest - 2/632D - Longest Subsequence.cpp
@@ -0,0 +1,13 @@
+//https://codeforces.com/problemset/problem/632/D
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    unordered_map<string, int> map;
+    map["string"] = 10;
+    cout <<map["string"];
+    return 0;
+}
